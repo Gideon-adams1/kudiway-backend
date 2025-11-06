be4991fc7aec430c0cbf67a7e1e410b83f885e77
Gideon Adams <ghcampusc@gmail.com>
2025-11-06 01:08:35 -0500

    Added static/media config and DEBUG env var

--- orders/apps.py
+++ orders/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
+import cloudinary
+
 
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+
+    def ready(self):
+        # ✅ Cloudinary manual configuration (runs when app starts)
+        cloudinary.config(
+            cloud_name="dmpymbirt",
+            api_key="356444953374757",
+            api_secret="a2Yvr9WNswF28a9K46HUeDV6DTk",
+        )
+        print("✅ Cloudinary initialized successfully for: dmpymbirt")
