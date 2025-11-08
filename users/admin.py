fbffc377c8219abdb1695c25f5c2f22ef8b1491b
Gideon Adams <ghcampusc@gmail.com>
2025-11-07 23:48:05 -0500

    Registered Profile and KudiPoints in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile, KudiPoints
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "is_verified_partner", "is_vendor")
+    list_filter = ("is_verified_partner", "is_vendor")
+    search_fields = ("user__username", "user__email")
+
+@admin.register(KudiPoints)
+class KudiPointsAdmin(admin.ModelAdmin):
+    list_display = ("user", "balance", "updated_at")
+    search_fields = ("user__username",)
